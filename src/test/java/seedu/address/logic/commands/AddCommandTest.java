a0d8afadb4f054c83e0b580e0092242fd5b453ad
timetraveller-123 <vishnuprasath639@gmail.com>
2023-10-24 15:52:01 +0800

    Fix tests

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -161,11 +161,6 @@ public class AddCommandTest {
         }
 
         @Override
-        public boolean hasOrder(Order order) {
-            throw new AssertionError("This method should not be called.");
-        }
-
-        @Override
         public void setOrder(Order target, Order editedOrder) {
             throw new AssertionError("This method should not be called.");
         }
@@ -176,7 +171,7 @@ public class AddCommandTest {
         }
 
         @Override
-        public Optional<Order> getOrder(int orderNumber) {
+        public Optional<Order> getOrder(String orderNumber) {
             throw new AssertionError("This method should not be called.");
         }
         @Override
--- src/test/java/seedu/address/testutil/TypicalOrders.java
+++ src/test/java/seedu/address/testutil/TypicalOrders.java
@@ -12,19 +12,19 @@ import seedu.address.model.order.Status;
  * A utility class containing a list of {@code Order} objects to be used in tests.
  */
 public class TypicalOrders {
-    public static final int ORDER_NUMBER_FIRST_ORDER = 1;
+    public static final String ORDER_NUMBER_FIRST_ORDER = "1";
 
-    public static final int ORDER_NUMBER_SECOND_ORDER = 1234;
     public static final Status STATUS_PENDING = new Status(Status.OrderStatus.PENDING);
     public static final Status STATUS_COMPLETED = new Status(Status.OrderStatus.COMPLETED);
+    public static final String ORDER_NUMBER_SECOND_ORDER = "1234";
 
-    public static final Order ALICE_PANADOL_ORDER = new Order(new OrderNumber("1"),
-            TypicalPersons.ALICE, "Panadol", STATUS_PENDING);
+    public static final Order CARL_PANADOL_ORDER = new Order(new OrderNumber("1"),
+            TypicalPersons.CARL, "Panadol", STATUS_PENDING);
 
     public static final Order BENSON_PANADOL_ORDER = new Order(new OrderNumber("1234"),
             TypicalPersons.BENSON, "Panadol", STATUS_COMPLETED);
-    public static final Order CARL_PANADOL_ORDER = new Order(new OrderNumber("1"),
-            TypicalPersons.CARL, "Panadol", STATUS_PENDING);
+    public static final Order ALICE_PANADOL_ORDER = new Order(new OrderNumber("1"),
+            TypicalPersons.ALICE, "Panadol", STATUS_PENDING);
 
     public static List<Order> getTypicalOrders() {
         return new ArrayList<>(Arrays.asList(CARL_PANADOL_ORDER, BENSON_PANADOL_ORDER));
