da9fb1dea90a5284f6bd884f6e794b53d856a613
timetraveller-123 <vishnuprasath639@gmail.com>
2023-10-25 17:42:01 +0800

    Add DeleteOrder class

--- src/main/java/seedu/address/model/order/exceptions/OrderNotFoundException.java
+++ src/main/java/seedu/address/model/order/exceptions/OrderNotFoundException.java
@@ -1,6 +1,8 @@
 package seedu.address.model.order.exceptions;
 
 /**
- * Signals that the operation is unable to find the specified Order.
+ * Signals that the operation is unable to find the specified person.
  */
 public class OrderNotFoundException extends RuntimeException {}
+
+
