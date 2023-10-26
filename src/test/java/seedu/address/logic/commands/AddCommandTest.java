d94405821a21bc4710a3deae0464e9c362dca259
timetraveller-123 <vishnuprasath639@gmail.com>
2023-10-27 00:55:17 +0800

    Fix rebase issues

--- src/test/java/seedu/address/logic/commands/AddCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddCommandTest.java
@@ -171,7 +171,7 @@ public class AddCommandTest {
         }
 
         @Override
-        public Optional<Order> getOrder(String orderNumber) {
+        public Optional<Order> getOrder(int orderNumber) {
             throw new AssertionError("This method should not be called.");
         }
         @Override
